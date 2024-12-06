78a729f9484fe588f4f984fd8a8fce1ebfc5cb28
Pouya1214 <pouyanaeimi12141214@gmail.com>
2024-12-07 00:31:07 +0100

    Add files via upload

---
+++ main2.go
@@ -0,0 +1,14 @@
+package main
+import ("fmt")
+
+func main() {
+  var b1 bool = true //madar ali jendas
+  var b2 = true // kos nane ali
+  var b3 bool // madar ali saleme
+  b4 := true // kos nane ali zarei
+
+  fmt.Println(b1) // Returns true
+  fmt.Println(b2) // Returns true
+  fmt.Println(b3) // Returns false
+  fmt.Println(b4) // Returns true
+}
---
+++ main3.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	myslice1 := []int{1, 2, 3, 4, 5, 6, 7}
+	fmt.Printf("myslice1 = %v\n", myslice1)
+	fmt.Printf("length = %d\n", len(myslice1))
+	fmt.Printf("capacity = %d\n", cap(myslice1))
+
+	myslice1 = append(myslice1, 20, 21, 23, 24)
+	fmt.Printf("myslice1 = %v\n", myslice1)
+	fmt.Printf("length = %d\n", len(myslice1))
+	fmt.Printf("capacity = %d\n", cap(myslice1))
+
+}
